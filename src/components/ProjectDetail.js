5a3d6a411db5527c8f9fb551632deacdc1e31211
Feeco <lifeike67@gmail.com>
2024-11-12 10:36:58 -0700

    ad

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -2,7 +2,7 @@ const index = (props) => {
   return (
     <div id="project">
       <div className="grid grid-cols-3">
-        <div> {props.title} </div>
+        <div className="font-bold"> {props.title} </div>
         <div> {props.description}</div>
         <div className="flex justify-end"> {props.date}</div>
       </div>
