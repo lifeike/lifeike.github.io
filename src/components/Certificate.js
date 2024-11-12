de3f664cf06d6302e875f6d166d2ed1ff896e348
Feeco <lifeike@gmail.com>
2024-11-12 13:13:57 -0700

    ad

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -2,7 +2,11 @@ const index = (props) => {
   return (
     <div id="certificate">
       <div className="flex justify-between">
-        <div className="font-medium text-blue-600 dark:text-blue-500 underline capitalize"> {props.title} </div>
+        <div className="font-medium text-blue-600 dark:text-blue-500 underline capitalize">
+          <a target="_blank" href="https://www.linkedin.com/posts/feeco_aws-saa-certificate-activity-7055594961253105664-K1ax">
+            {props.title}
+          </a>
+        </div>
         <div> {props.description}</div>
       </div>
     </div>
--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -4,7 +4,13 @@ const index = (props) => {
       <div className="grid grid-cols-3">
         <div className="font-bold"> {props.title} </div>
         <div> {props.description}</div>
-        <div className="flex justify-end"> {props.date}</div>
+        <div className="flex justify-end">
+          {props.links.map((item, index) => (
+            <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" target="_blank" href={item.url} key={item.url}>
+              {item.label}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   )
